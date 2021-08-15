544a01b1db62100d81568cc9d1a31bab9e842662
Nicolas Combe <combe98@gmail.com>
2021-08-14 23:10:04 -0500

    Testing and fixing workflows

--- GraphQL-ExampleTests/GraphQL_ExampleTests.swift
+++ GraphQL-ExampleTests/GraphQL_ExampleTests.swift
@@ -8,8 +8,7 @@
 import XCTest
 @testable import GraphQL_Example
 
-class GraphQL_ExampleTests: XCTestCase {
-
+final class GraphQL_ExampleTests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -18,16 +17,7 @@ class GraphQL_ExampleTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testReality() {
+        XCTAssertEqual(1 + 1, 2)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
